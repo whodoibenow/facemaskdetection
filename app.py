2895625f86aa30bd731ee1e8fb4b6d42616d2eb3
Masooque Ali Siddiqui <69395065+whodoibenow@users.noreply.github.com>
2021-09-10 20:44:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,6 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
